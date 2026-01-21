cb851e20870711d6ca40c0ea5ac3d0e631190e81
John <Sapo@gmail.com>
2026-01-21 14:52:41 -0500

    timer y try-catch

---
+++ Algoritmos/timer.java
@@ -0,0 +1,7 @@
+package Algoritmos;
+
+public class timer {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Algoritmos/tryCatch.java
@@ -0,0 +1,35 @@
+package Algoritmos;
+
+import java.util.Scanner;
+
+public class tryCatch {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Ingrese el dividendo");
+            double dividendo = sc.nextDouble();
+        
+            System.out.println("Ingrese el divisor");
+            double divisor = sc.nextDouble();
+        
+            if (divisor == 0) {
+                throw new ArithmeticException();
+            }
+        
+            double resultado = dividendo / divisor;
+            System.out.println("Resultado: " + resultado);
+        
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir entre 0");
+        } catch (Exception e) {
+            System.out.println("Entrada inválida");
+        } finally {
+            sc.close();
+            System.out.println("Fin del programa");
+        }
+        
+
+    }
+}
