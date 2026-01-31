7f8786806431f0aa3ad7b3f407425b2cba3baf8c
John <john@MacBook-Pro-de-JOHN.local>
2026-01-31 07:41:13 -0500

    crear base de datos

---
+++ BaseDatos/PrimerEjerciciosBaseDatos.sql
@@ -0,0 +1,17 @@
+/*crear base de datas y usarla para creacion de talbas*/
+CREATE DATABASE crear_DB;
+USE crear_DB;
+
+/*crear tabla*/
+CREATE TABLE personas(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    edad INT
+);
+
+/*insertar datos*/
+INSERT INTO personas(nombre, edad)
+VALUES ("Juan", 17);
+
+/*ver datos*/
+SELECT * FROM personas;
